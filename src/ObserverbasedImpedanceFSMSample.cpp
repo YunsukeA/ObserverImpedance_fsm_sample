a2d4ccaeb5c1fbfa6f7da88e579a18267638b402
YunosukeA22 <aoforuniversity@gmail.com>
2025-07-29 17:25:41 +0900

    initial commit

--- src/ObserverbasedImpedanceFSMSample.cpp
+++ src/ObserverbasedImpedanceFSMSample.cpp
@@ -1,20 +1,39 @@
 #include "ObserverbasedImpedanceFSMSample.h"
 
-ObserverbasedImpedanceFSMSample::ObserverbasedImpedanceFSMSample(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
-: mc_control::fsm::Controller(rm, dt, config)
-{
+ObserverbasedImpedanceFSMSample::ObserverbasedImpedanceFSMSample(
+    mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration &config)
+    : mc_control::fsm::Controller(rm, dt, config) {
+  datastore().make_call("KinematicAnchorFrame::" + robot().name(),
+                        [](const mc_rbdyn::Robot &robot) {
+                          return sva::interpolate(
+                              robot.surfacePose("LeftFoot"),
+                              robot.surfacePose("RightFoot"), 0.5);
+                        });
+
+  auto stabiConf = robot().module().defaultLIPMStabilizerConfiguration();
+
+  lipm_stabilizer_ptr_ =
+      std::make_shared<mc_tasks::lipm_stabilizer::StabilizerTask>(
+          solver().robots(), solver().realRobots(), robots().robotIndex(),
+          stabiConf.leftFootSurface, stabiConf.rightFootSurface,
+          stabiConf.torsoBodyName, solver().dt());
+  lipm_stabilizer_ptr_->reset();
+  lipm_stabilizer_ptr_->setContacts(
+      {mc_tasks::lipm_stabilizer::ContactState::Left,
+       mc_tasks::lipm_stabilizer::ContactState::Right});
+
+  lipm_stabilizer_ptr_->configure(stabiConf);
+  solver().addTask(lipm_stabilizer_ptr_);
 
   mc_rtc::log::success("ObserverbasedImpedanceFSMSample init done ");
 }
 
-bool ObserverbasedImpedanceFSMSample::run()
-{
+bool ObserverbasedImpedanceFSMSample::run() {
   return mc_control::fsm::Controller::run();
 }
 
-void ObserverbasedImpedanceFSMSample::reset(const mc_control::ControllerResetData & reset_data)
-{
+void ObserverbasedImpedanceFSMSample::reset(
+    const mc_control::ControllerResetData &reset_data) {
+  lipm_stabilizer_ptr_.reset();
   mc_control::fsm::Controller::reset(reset_data);
 }
-
-
--- src/ObserverbasedImpedanceFSMSample.h
+++ src/ObserverbasedImpedanceFSMSample.h
@@ -1,15 +1,21 @@
 #pragma once
 
-#include <mc_control/mc_controller.h>
 #include <mc_control/fsm/Controller.h>
+#include <mc_control/mc_controller.h>
+#include <mc_tasks/lipm_stabilizer/StabilizerTask.h>
 
 #include "api.h"
 
-struct ObserverbasedImpedanceFSMSample_DLLAPI ObserverbasedImpedanceFSMSample : public mc_control::fsm::Controller
-{
-  ObserverbasedImpedanceFSMSample(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
+struct ObserverbasedImpedanceFSMSample_DLLAPI ObserverbasedImpedanceFSMSample
+    : public mc_control::fsm::Controller {
+  ObserverbasedImpedanceFSMSample(mc_rbdyn::RobotModulePtr rm, double dt,
+                                  const mc_rtc::Configuration &config);
 
   bool run() override;
 
-  void reset(const mc_control::ControllerResetData & reset_data) override;
+  void reset(const mc_control::ControllerResetData &reset_data) override;
+
+private:
+  std::shared_ptr<mc_tasks::lipm_stabilizer::StabilizerTask>
+      lipm_stabilizer_ptr_;
 };
